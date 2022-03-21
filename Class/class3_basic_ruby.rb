1b9dbdba930e39b4b5b4ba25a6a2c6e3a63a9fd4
Pecximenes <pedroximenes76@gmail.com>
2022-03-21 16:25:50 -0300

    Update: Finished class3_basic_ruby.rb

--- Class/class3_basic_ruby.rb
+++ Class/class3_basic_ruby.rb
@@ -61,3 +61,85 @@ w = p && q # Sinal lógico e (&& ou and)
 x = r || s # Sinal lógico ou (|| ou or)
 y = t ^ u # Sinal lógico ou exclusivo
 z = ! v # Sinal lógico negado (! ou not)
+
+
+#* Estrutura de Decisão - Condicionais
+# Estrutura que verifica a validade verdadeira de uma condição para poder executar um determinado conjunto de código
+
+if (j > o)
+    puts "j é maior que o!"
+elsif (j == o)
+    puts "j é igual a o!"
+else
+    puts "j é menor que o!"
+end
+
+
+#* Estrutura de Decisão - Unless
+# Estrutura oposta ao if/elsif/else, onde execulta quando a validade é falsa de uma condição, semelhante ao "if not"
+#? Existe uma estrutura semelhante ao elsif/else no caso do unless?
+
+unless l < 100
+    puts "l é maior que 100"
+end
+unless l < 1
+    puts "l é maior que 1"
+end
+
+
+#* Estrutura de Decisão - Case
+# Semelhante a condição if, porém com uso focado em multiplas condições
+
+print "Informe a sua idade:"
+idade = gets.to_i
+case idade
+when 0..12
+    puts "Você é uma criança"
+when 13..17
+    puts "Você é um adolescente"
+else
+    puts "Você é um adulto"
+end
+
+
+#* Estruturas de Repetição - while
+
+contador = 0
+while contador < 10
+    puts contador
+    contador += 1
+end
+
+
+#* Estruturas de Repetição - until
+
+contador = 15
+until contador == 0
+    puts contador
+    contador -= 1
+end
+
+
+#* Estruturas de Repetição - for
+# Acrescetnta o método .step para indicar quantos passos deseja-se fazer
+
+for i in (4..13).step(3)
+    puts i
+end
+
+
+#* Funções
+# Para criar uma variável global, é necessário um @ antes da variável
+
+@numero = 21
+def isPar?(numero)
+    if numero % 2 == 0
+        @numero -= 5
+        return true
+    else
+        @numero += 3
+        return false
+    end
+end
+puts isPar?(@numero)
+puts @numero
